001a7ae7a4c66e1fa01f4a6d83d3fb2ae1ebc713
aboytard <alban.boytard@atlascopco.com>
2024-01-09 09:55:54 +0100

    data structure for the consumer and publisher in rabbitmq

---
+++ CoreExample/AspNetServiceLib/DataStructures/MQConsumerParams.cs
@@ -0,0 +1,18 @@
+﻿namespace AspNetServiceLib.DataStructures
+{
+    public struct MQConsumerParams
+    {
+        public bool UseAck { get; }
+        public string ExchangeType { get; }
+        public bool UsePublicQueue { get; }
+        public bool ExplicitStart { get; }
+
+        public MQConsumerParams(bool useAck, string exchangeType, bool usePublicQueue, bool explicitStart)
+        {
+            UseAck = useAck;
+            ExchangeType = exchangeType;
+            UsePublicQueue = usePublicQueue;
+            ExplicitStart = explicitStart;
+        }
+    }
+}
---
+++ CoreExample/AspNetServiceLib/DataStructures/MQPublisherParams.cs
@@ -0,0 +1,18 @@
+﻿namespace AspNetServiceLib.DataStructures
+{
+    public struct MQPublisherParams
+    {
+        public bool WaitForDelivery { get; }
+        public string ExchangeType { get; }
+        public bool UsePublicQueue { get; }
+        public bool UsesReplies { get; }
+
+        public MQPublisherParams(bool waitForDelivery, string exchangeType, bool usePublicQueue, bool usesReplies)
+        {
+            WaitForDelivery = waitForDelivery;
+            ExchangeType = exchangeType;
+            UsePublicQueue = usePublicQueue;
+            UsesReplies = usesReplies;
+        }
+    }
+}
